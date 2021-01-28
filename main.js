6cb4c744cf28fb1ad13ca2609a92fbc009aaac3a
NBryant83 <nakisha.bryant@gmail.com>
2021-01-28 16:47:38 -0500

    js_selectors

--- main.js
+++ main.js
@@ -3,11 +3,33 @@ console.log('successfully linked!')
 // For each prompt, perform the specified selection. Save the result to a variable, then console.log that variable. Confirm that it contains what it should in the console!
 
 // 1) All img tags
-// 2) All section tags
-// 3) The element with an id of section-1
-// 4) The element with an id of destinations-button
-// 5) The first element with a class of images-rack
-// 6) The first h1 tag
-// 7) All elements with a class of image-card
-// 8) All elements with a class of over-image-text
+const imgs = document.querySelectorAll('img')
+console.log(imgs);
 
+// 2) All section tags
+const sections = document.querySelectorAll('section')
+console.log(sections);
+
+// 3) The element with an id of section-1
+const sectionOne = document.getElementById('section-1')
+console.log(sectionOne);
+
+// 4) The element with an id of destinations-button
+const button = document.getElementById('destinations-button')
+console.log(button);
+
+// 5) The first element with a class of images-rack
+const rackImages = document.querySelector('.images-rack')
+console.log(rackImages);
+
+// 6) The first h1 tag
+const tag = document.querySelector('h1')
+console.log(tag);
+
+// 7) All elements with a class of image-card
+const cardImage = document.getElementsByClassName('image-card')
+console.log(cardImage);
+
+// 8) All elements with a class of over-image-text
+const overImageText = document.getElementsByClassName('over-image-text')
+console.log(overImageText);
